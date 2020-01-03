4ebbc394ec3f45c66bcba92764fadc916f0769e4
mcdobr <mircea.dobreanu@gmail.com>
2020-01-03 14:46:40 +0200

    Fix time bugs

--- main.c
+++ main.c
@@ -10,12 +10,17 @@ static const int TIME_SYNC_TAG = 3;
 
 static const char *poll_message = "TIME_POLL";
 
+static double start_time;
 
-void slave_reply_to_poll(double start_time, int world_rank);
+double current_time() {
+    return MPI_Wtime() - start_time;
+}
+
+void slave_reply_to_poll(int world_rank);
 
 void slave_wait_for_master_poll(int world_rank);
 
-void slave_send_current_time(double start_time, int world_rank);
+void slave_send_current_time(int world_rank);
 
 void master_synchronize_clocks(int world_size, int world_rank);
 
@@ -25,10 +30,11 @@ void slave_wait_for_adjustment(int world_rank);
 
 void master_broadcast_poll(int world_size);
 
-int main(int argc, char *argv[]) {
-    double start_time = MPI_Wtime();
+void print_array(int number_of_elements, const double *array);
 
+int main(int argc, char *argv[]) {
     MPI_Init(&argc, &argv);
+    start_time = MPI_Wtime();
 
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
@@ -38,16 +44,16 @@ int main(int argc, char *argv[]) {
 
     if (world_rank == MASTER_RANK) {
         master_broadcast_poll(world_size);
-        slave_reply_to_poll(start_time, world_rank);
+        slave_reply_to_poll(world_rank);
 
         master_synchronize_clocks(world_size, world_rank);
         slave_wait_for_adjustment(world_rank);
     } else {
-        slave_reply_to_poll(start_time, world_rank);
+        slave_reply_to_poll(world_rank);
         slave_wait_for_adjustment(world_rank);
     }
 
-    printf("Process [%d] exited normally\n", world_rank);
+    printf("Process [%d]: exited normally\n", world_rank);
 
     MPI_Finalize();
     return 0;
@@ -62,17 +68,18 @@ void master_broadcast_poll(int world_size) {
     printf("Master process: Sent all poll messages\n");
 }
 
-void slave_reply_to_poll(double start_time, int world_rank) {
+void slave_reply_to_poll(int world_rank) {
     slave_wait_for_master_poll(world_rank);
-    slave_send_current_time(start_time, world_rank);
+    slave_send_current_time(world_rank);
 }
 
 void slave_wait_for_adjustment(int world_rank) {
     printf("Process [%d]: waiting to receive delta from master\n", world_rank);
+
     double delta;
     MPI_Status status;
     MPI_Recv(&delta, 1, MPI_DOUBLE, MASTER_RANK, TIME_SYNC_TAG, MPI_COMM_WORLD, &status);
-    printf("Process [%d] received a delta of %.4lf\n", world_rank, delta);
+    printf("Process [%d] received a delta of %.6lf to be added to local time %lf\n", world_rank, delta, current_time());
 }
 
 void slave_wait_for_master_poll(int world_rank) {
@@ -86,9 +93,9 @@ void slave_wait_for_master_poll(int world_rank) {
     buffer = NULL;
 }
 
-void slave_send_current_time(double start_time, int world_rank) {
-    double elapsed_time = MPI_Wtime() - start_time;
-    printf("Process [%d] sending current time which is %lf seconds (from start of program) to master [%d]\n", world_rank, elapsed_time, MASTER_RANK);
+void slave_send_current_time(int world_rank) {
+    double elapsed_time = current_time();
+    printf("Process [%d]: sending current time which is %lf seconds (from start of program) to master [%d]\n", world_rank, elapsed_time, MASTER_RANK);
     MPI_Send(&elapsed_time, 1, MPI_DOUBLE, MASTER_RANK, TIME_GATHER_TAG, MPI_COMM_WORLD);
 }
 
@@ -106,11 +113,17 @@ void master_synchronize_clocks(int world_size, int world_rank) {
         MPI_Recv(&times_received[source_rank], 1, MPI_DOUBLE, source_rank, TIME_GATHER_TAG, MPI_COMM_WORLD, &status);
         printf("Process [%d]: Received time %lf\n", world_rank, times_received[source_rank]);
 
-
-        round_trip_times[source_rank] = MPI_Wtime() - times_received[source_rank];
+        round_trip_times[source_rank] = current_time() - times_received[source_rank];
     }
 
-    double correct_time = average(world_size, round_trip_times);
+    printf("Times received: ");
+    print_array(world_size, times_received);
+
+    printf("Round trip times: ");
+    print_array(world_size, round_trip_times);
+
+    double correct_time = average(world_size, times_received);
+    printf("Master [%d]: The correct time was %lf\n", MASTER_RANK, correct_time);
 
     for (int dest_rank = 0; dest_rank < world_size; ++dest_rank) {
         double delta = correct_time - times_received[dest_rank] + round_trip_times[dest_rank];
@@ -125,17 +138,16 @@ void master_synchronize_clocks(int world_size, int world_rank) {
 }
 
 double average(int number_of_elements, const double *array) {
-    for (int i = 0; i < number_of_elements; ++i) {
-        printf("%10.4lf ", array[i]);
-    }
-    printf("\n");
-
-
-
-
     double result = 0.0;
     for (int i = 0; i < number_of_elements; ++i) {
         result += 1.0 / number_of_elements * array[i];
     }
     return result;
+}
+
+void print_array(int number_of_elements, const double *array) {
+    for (int i = 0; i < number_of_elements; ++i) {
+        printf("%10.6lf ", array[i]);
+    }
+    printf("\n");
 }
